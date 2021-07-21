3f0ef784a75efe35f676cfd81432188fea8b5f2d
wenlin <569958268@qq.com>
2021-07-21 15:46:56 +0800

    chore: 删除多余的style rule

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,31 +1,9 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   globals: {},
   extends: ["./node_modules/@ml/lint/.stylelintrc.js"],
-  rules: {
-    "value-keyword-case": null,
-    "at-rule-no-unknown": [true, {
-      "ignoreAtRules": ["function", "if", "else", "each", "for", "include", "mixin"]
-    }],
-    "property-no-unknown": [
-      true,
-      {
-        "ignoreProperties": [
-          "composes"
-        ]
-      }
-    ],
-    "selector-pseudo-class-no-unknown": [
-      true,
-      {
-        "ignorePseudoClasses": [
-          "global"
-        ]
-      }
-    ]
-
-  }
-}
+  rules: {},
+};
