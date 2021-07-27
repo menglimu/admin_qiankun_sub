0c368d55fba97e4c6d0b2725dfd4aefaef0b8b90
wenlin <569958268@qq.com>
2021-07-27 15:19:21 +0800

    feat: 播放音频 playAudio

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -12,13 +12,13 @@
  * @param time 需要格式的日期
  * @param format 格式化的格式
  */
-export function parseTime(time: Date | number | string, format = 'yyyy-mm-dd hh:ii:ss') {
-  if (!time) return '';
+export function parseTime(time: Date | number | string, format = "yyyy-mm-dd hh:ii:ss") {
+  if (!time) return "";
   let date: Date;
   if (time instanceof Date) {
     date = time;
   } else {
-    let _time = typeof time === 'string' ? parseInt(time, 10) : time;
+    let _time = typeof time === "string" ? parseInt(time, 10) : time;
     if (String(_time).length === 10) _time = _time * 1000;
     date = new Date(_time);
   }
@@ -29,13 +29,13 @@ export function parseTime(time: Date | number | string, format = 'yyyy-mm-dd hh:
     h: date.getHours(),
     i: date.getMinutes(),
     s: date.getSeconds(),
-    a: date.getDay()
+    a: date.getDay(),
   };
   const time_str = format.replace(/(y|m|d|h|i|s|a)+/gi, (result, key) => {
     let value = formatObj[key];
-    if (key === 'a') return ['一', '二', '三', '四', '五', '六', '日'][value - 1];
+    if (key === "a") return ["一", "二", "三", "四", "五", "六", "日"][value - 1];
     if (result.length > 0 && value < 10) {
-      value = '0' + value;
+      value = "0" + value;
     }
     return value || 0;
   });
@@ -57,19 +57,19 @@ export function formatTime(time: string | number | Date, format?: string) {
   const diff = (Date.now() - date.getTime()) / 1000;
 
   if (diff < 30) {
-    return '刚刚';
+    return "刚刚";
   } else if (diff < 3600) {
     // less 1 hour
-    return Math.ceil(diff / 60) + '分钟前';
+    return Math.ceil(diff / 60) + "分钟前";
   } else if (diff < 3600 * 24) {
-    return Math.ceil(diff / 3600) + '小时前';
+    return Math.ceil(diff / 3600) + "小时前";
   } else if (diff < 3600 * 24 * 2) {
-    return '1天前';
+    return "1天前";
   }
   if (format) {
     return parseTime(date, format);
   } else {
-    return date.getMonth() + 1 + '月' + date.getDate() + '日' + date.getHours() + '时' + date.getMinutes() + '分';
+    return date.getMonth() + 1 + "月" + date.getDate() + "日" + date.getHours() + "时" + date.getMinutes() + "分";
   }
 }
 
@@ -83,10 +83,10 @@ export function GetQueryString(name: string) {
   // const r = window.location.search.substr(1).match(reg)
   // if (r != null) return r[2]
   // return null
-  const reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
+  const reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
   let search = window.location.search;
   if (!search) {
-    search = `?${window.location.href.split('?')[1]}`;
+    search = `?${window.location.href.split("?")[1]}`;
   }
   const r = search.substr(1).match(reg);
   if (r) return r[2];
@@ -95,7 +95,7 @@ export function GetQueryString(name: string) {
 
 // TODO: 完善
 // eslint-disable-next-line max-params
-export function findValByKey(data: any, val: any, key = 'id', childrenKey = 'children'): any {
+export function findValByKey(data: any, val: any, key = "id", childrenKey = "children"): any {
   // for (const i in data) {
   //   if (data[i][key] && data[i][key] === val) {
   //     return data[i];
@@ -112,12 +112,14 @@ export function findValByKey(data: any, val: any, key = 'id', childrenKey = 'chi
  */
 export function createRandomId() {
   return (
-    'id-' +
+    "id-" +
     (Math.random() * 10000000).toString(16).substr(0, 4) +
-    '-' +
+    "-" +
     new Date().getTime() +
-    '-' +
-    Math.random().toString().substr(2, 5)
+    "-" +
+    Math.random()
+      .toString()
+      .substr(2, 5)
   );
 }
 
@@ -127,16 +129,16 @@ export function createRandomId() {
  * @return {Boolean} 是否为空
  */
 export function isNull(str: any): boolean {
-  if (str === undefined || str === null || str === '') {
+  if (str === undefined || str === null || str === "") {
     return true;
   } else if (Array.isArray(str) && str.length === 0) {
     return true;
-  } else if (typeof str === 'object' && Object.keys(str).length === 0) {
+  } else if (typeof str === "object" && Object.keys(str).length === 0) {
     return true;
   }
   return false;
 }
-/**
+/**   TODO: 优化 in
  * @description: 判断一个对象时候符合judge的条件。所有值相等是为true，任一值不相等返回false
  * @param {AnyObj} judge 条件
  * @param {AnyObj} data 数据
@@ -149,4 +151,20 @@ export function getJudge(judge: AnyObj, data: AnyObj): boolean {
     }
   }
   return true;
+}
+/**
+ * 直接播放语音提示
+ * @param url 语音地址 或者 require('xxx')
+ * @param loop 循环播放
+ * @returns HTMLAudioElement
+ */
+export function playAudio(url: string, loop = false) {
+  const audio = new Audio(url);
+  audio.play();
+  if (loop) {
+    audio.onended = () => {
+      audio.play();
+    };
+  }
+  return audio;
 }
