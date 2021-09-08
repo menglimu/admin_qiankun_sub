1cda77cf68e322ad2b726bddccce2570474a9231
wenlin <569958268@qq.com>
2021-09-08 09:37:56 +0800

    feat: 增加脚本

---
+++ build/baseClone.sh
@@ -0,0 +1,3 @@
+git remote add -f base ssh://git@git.citycloud.com.cn:3022/hcsg_code/web_template_qiankun_sub.git
+git branch base remotes/base/master
+# git checkout -b base remotes/base/master
---
+++ build/baseUpdate.sh
@@ -0,0 +1,2 @@
+git fetch base
+git merge base
