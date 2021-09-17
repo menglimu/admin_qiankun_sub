ade07288ac97f39c0b97545bf05d9ba10382e0c6
wenlin <569958268@qq.com>
2021-09-17 17:07:19 +0800

    chore: clone

--- build/baseClone.sh
+++ build/baseClone.sh
@@ -1,3 +1,3 @@
-git remote add -f base ssh://git@github.com:menglimu/admin_qiankun_sub.git
+git remote add -f base git@github.com:menglimu/admin_qiankun_sub.git
 git branch base remotes/base/master
 # git checkout -b base remotes/base/master
