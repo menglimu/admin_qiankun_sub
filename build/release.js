b8b86e8916633054c55535416777e1cc8d70579c
wenlin <569958268@qq.com>
2021-12-23 10:37:39 +0800

    feat: 修改版本脚本

--- build/release.js
+++ build/release.js
@@ -1,7 +1,5 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 const fs = require("fs");
-
-const readFileSync = require("fs").readFileSync;
 const execSync = require("child_process").execSync; // 同步子进程
 const join = require("path").join;
 
@@ -14,7 +12,7 @@ const log = execSync("git log -1")
     return item.trim();
   });
 
-const branch = execSync("git branch -vv")
+const branch = execSync("git branch")
   .toString()
   .trim()
   .split("\n")
@@ -22,21 +20,21 @@ const branch = execSync("git branch -vv")
   .map(item => {
     return item.trim();
   });
-
+const nowBranch = branch.find(item => item.indexOf("*") === 0) || branch[0];
 const outJsonDefault = {
   commit: log[0].replace("commit", "").trim(),
-  commitTime: new Date(log[2].replace("Date:", "").trim()).toLocaleString(),
+  commitTime: new Date(log[3].replace("Date:", "").trim()).toLocaleString(),
   lastComment: execSync("git show -s --format=%s")
     .toString()
     .trim(),
-  branch: branch[0],
-  lastAuthor: log[1].replace("Author: ", ""),
+  branch: nowBranch?.replace("* ", ""),
+  lastAuthor: log[2].replace("Author: ", ""),
   buildDateTime: new Date().toLocaleString(),
   gitAddress: execSync("git remote -v")
     .toString()
     .trim()
     .split("\n")
-    .join(" , ")
+    .join(" , "),
 };
 console.log("写入版本信息");
 // 写入版本信息
