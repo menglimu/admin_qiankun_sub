d68682479afe78b1da2c518c4ac52ee8a38287e5
wenlin <569958268@qq.com>
2022-02-07 16:57:58 +0800

    feat: 修改菜单

--- src/router/static.ts
+++ src/router/static.ts
@@ -5,12 +5,12 @@ export const staticRoutes: FunItem[] = [
   {
     id: createRandomId(),
     text: "外链",
-    url: "https://www.baidu.com"
+    url: "https://www.baidu.com",
   },
   {
     id: createRandomId(),
     text: "首页",
-    url: "/home"
+    url: "/home",
   },
   {
     id: createRandomId(),
@@ -20,37 +20,37 @@ export const staticRoutes: FunItem[] = [
       {
         id: createRandomId(),
         text: "cache缓存",
-        url: "/cache",
-        icon: "avatar"
+        url: "/test",
+        icon: "avatar",
       },
       {
         id: createRandomId(),
         text: "root下的菜单",
-        url: "/root"
+        url: "/root",
       },
       {
         id: createRandomId(),
         text: "外链",
         url: "https://www.baidu.com",
-        orderNo: 3
+        orderNo: 3,
       },
       {
         id: createRandomId(),
         text: "相对地址外链",
         url: "/http//12325",
-        orderNo: 1
+        orderNo: 1,
       },
       {
         id: createRandomId(),
         text: "iframe",
         url: "/iframe/https://www.baidu.com",
-        orderNo: 2
+        orderNo: 2,
       },
       {
         id: createRandomId(),
         text: "隐藏菜单",
         url: "/hide",
-        nodeType: 3
+        nodeType: 3,
       },
       {
         id: createRandomId(),
@@ -62,9 +62,9 @@ export const staticRoutes: FunItem[] = [
             id: createRandomId(),
             text: "按钮",
             url: "add",
-            nodeType: 3
-          }
-        ]
+            nodeType: 3,
+          },
+        ],
       },
       {
         id: createRandomId(),
@@ -74,15 +74,15 @@ export const staticRoutes: FunItem[] = [
           {
             id: createRandomId(),
             text: "菜单1",
-            url: "/add1"
+            url: "/add1",
           },
           {
             id: createRandomId(),
             text: "菜单2",
-            url: "/add2"
-          }
-        ]
-      }
-    ]
-  }
+            url: "/add2",
+          },
+        ],
+      },
+    ],
+  },
 ];
---
+++ src/views/test/index.tsx
@@ -0,0 +1,17 @@
+/**
+ * @Author: wenlin
+ * @Description:
+ */
+import Vue from "vue";
+import styles from "./index.module.scss";
+
+export default Vue.extend({
+  name: "test",
+  data() {
+    return {};
+  },
+  methods: {},
+  render() {
+    return <div>测试页</div>;
+  },
+});
